bb1f144cbf408b482034985a71e8a202010fc3c1
Daniil Petrov <ms13@.com>
2023-05-08 17:54:26 +0500

    task 003

--- task001/Program.cs
+++ task001/Program.cs
@@ -5,3 +5,22 @@
 // 23432 -> да
 
 
+
+
+Console.Write("Введите число =  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n= number, dig;
+
+int rev = 0;
+while (n >0)
+{
+    dig = n % 10;
+    rev = rev * 10+ dig;
+}
+if (n == rev)
+{
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}
